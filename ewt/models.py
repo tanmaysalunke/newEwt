cac022fe86ab9c03ad007869463783dea8f76df0
Tanmay <tanmay44200@gmail.com>
2022-03-19 23:19:43 +0530

    username does not exist

--- ewt/models.py
+++ ewt/models.py
@@ -13,3 +13,12 @@ class CustomUser(AbstractUser):
     company_name = models.CharField(max_length=150)
     location = models.CharField(max_length=150)
     category = models.CharField(choices=CATEGORY, max_length=50, default='Manufacturer')
+
+class manu_data(models.Model):
+    display_uid = models.IntegerField(max_length=4)
+    ram_uid = models.IntegerField(max_length=4)
+    hdd_uid = models.IntegerField(max_length=4)
+    ssd_uid = models.IntegerField(max_length=4)
+    processor_uid = models.IntegerField(max_length=4)
+    graphics_uid = models.IntegerField(max_length=4)
+    battery_uid = models.IntegerField(max_length=4)
--- ewt/views.py
+++ ewt/views.py
@@ -1,7 +1,8 @@
 from unicodedata import category
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import Group
-from django.db import IntegrityError
+from django.db import IntegrityError, models
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
 from .models import CustomUser
 from django.contrib.auth.decorators import login_required
@@ -33,31 +34,37 @@ def loginPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = CustomUser.objects.get(username=username)
+        try:
+            user = CustomUser.objects.get(username=username)
+            name = user.first_name
+            if check_password(password, user.password) is True:
+                login(request, user)
+                #category based access to pages:
+                if user.category == 'Manufacturer':
+                    print(uid_dict.display_type)
+                    return render(request, 'ewt/Manufacturer.html', {'data' : uid_dict.display_type, 'data1': uid_dict.display_spec, 
+                                                                    'data2': uid_dict.ram_type,'data3': uid_dict.ram_spec, 
+                                                                    'data4' : uid_dict.hdd_type, 'data5': uid_dict.hdd_spec, 
+                                                                    'data6': uid_dict.ssd_type, 'data7' : uid_dict.ssd_spec, 
+                                                                    'data8': uid_dict.processor_type, 'data9': uid_dict.processor_spec, 
+                                                                    'data10' : uid_dict.graphics_type, 'data11': uid_dict.graphics_spec, 
+                                                                    'data12': uid_dict.battery_type, 'data13': uid_dict.battery_spec})
+                elif user.category == 'Refurbisher':
+                    return render(request, 'ewt/Refurbisher.html', {'user_name' : name})
+                elif user.category == 'Recycler':
+                    return render(request, 'ewt/recycler.html', {'user_name' : name})
+            else:
+                messages.info(request, 'Username or Password is incorrect')
 
-        name = user.first_name
+        except ObjectDoesNotExist:
+            messages.info(request, "Username does not exist!")
+
+        
         # print(user.category)
 
         # implement try catch for username validation
-
-        if check_password(password, user.password) is True:
-            login(request, user)
-            #category based access to pages:
-            if user.category == 'Manufacturer':
-                print(uid_dict.display_type)
-                return render(request, 'ewt/Manufacturer.html', {'data' : uid_dict.display_type, 'data1': uid_dict.display_spec, 
-                                                                'data2': uid_dict.ram_type,'data3': uid_dict.ram_spec, 
-                                                                'data4' : uid_dict.hdd_type, 'data5': uid_dict.hdd_spec, 
-                                                                'data6': uid_dict.ssd_type, 'data7' : uid_dict.ssd_spec, 
-                                                                'data8': uid_dict.processor_type, 'data9': uid_dict.processor_spec, 
-                                                                'data10' : uid_dict.graphics_type, 'data11': uid_dict.graphics_spec, 
-                                                                'data12': uid_dict.battery_type, 'data13': uid_dict.battery_spec})
-            elif user.category == 'Refurbisher':
-                return render(request, 'ewt/Refurbisher.html', {'user_name' : name})
-            elif user.category == 'Recycler':
-                return render(request, 'ewt/recycler.html', {'user_name' : name})
-        else:
-            messages.info(request, 'Username or Password is incorrect')
+        
+        
     return render(request, 'ewt/login.html')
 
 
@@ -67,3 +74,5 @@ def logoutUser(request):
     logout(request)
     return redirect('login')
 
+
+# user_reg = manu_data(request.POST.get(display),0104,0104,0104,0104,0104,0104)
